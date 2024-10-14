e7361b74d6feaca1b7bc9f02ccf740a830a8beba
Simanga <simacoder@hotmail>
2024-10-14 21:33:02 +0200

    updated readme file, finalize todo component and backend json server

--- todolist/src/app/app.routes.ts
+++ todolist/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { TodoComponent } from './todo/todo.component';
 
 export const routes: Routes = [
     {path:'', redirectTo:'login', pathMatch:'full'},
     {path:'login', component:LoginComponent},
-    {path:'signup', component:SignupComponent}
+    {path:'signup', component:SignupComponent},
+    {path:'todo',component:TodoComponent}
 ];
